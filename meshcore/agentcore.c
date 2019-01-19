2d0e8d227f61f6dd0465eca8d2be468a60f4f3b4
Bryan Roe <git.bryan.roe@techsavvydude.com>
2019-01-18 19:22:24 -0800

    Updated endianess of AgentCapabilities

--- meshcore/agentcore.c
+++ meshcore/agentcore.c
@@ -3241,6 +3241,7 @@ int MeshAgent_AgentMode(MeshAgentHostContainer *agentHost, int paramLen, char **
 		if (ILibSimpleDataStore_Get(agentHost->masterDb, "AgentCapabilities", NULL, 0) == 4)
 		{
 			ILibSimpleDataStore_Get(agentHost->masterDb, "AgentCapabilities",(char*) &dbCapabilities, 4);
+			dbCapabilities = (int)ntohl(dbCapabilities);
 			agentHost->capabilities |= dbCapabilities;
 		}
 	}
