c0fcf75f58077334adfd7e6c6bde0ce270962eb0
Bryan Roe <git.bryan.roe@techsavvydude.com>
2019-08-05 11:12:00 -0700

    Fixed bug, where stderr was not subscribed, causing the stream to pause if stderr is written to, which will happen on Win Server when fetching AV state. If stream is paused, the waitExit() line will hang

--- modules/win-info.js
+++ modules/win-info.js
@@ -44,6 +44,7 @@ function av()
 {
     var child = require('child_process').execFile(process.env['windir'] + '\\System32\\wbem\\wmic.exe', ['wmic', '/Namespace:\\\\root\\SecurityCenter2', 'Path', 'AntiVirusProduct', 'get', '/FORMAT:CSV']);
     child.stdout.str = ''; child.stdout.on('data', function (c) { this.str += c.toString(); });
+    child.stderr.str = ''; child.stderr.on('data', function (c) { this.str += c.toString(); })
     child.waitExit();
 
     var lines = child.stdout.str.trim().split('\r\n');
