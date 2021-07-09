2a2386b78e4a6ea591d7b192957377d5f2e70d9f
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2021-07-09 12:40:53 -0700

    Fixed agent install dialog text.

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2021-Jul-6 14:07:36-0700"  
-#define SOURCE_COMMIT_HASH "1f5022ed166093f3781ce05814d4e1a5d6b3b625"  
+#define SOURCE_COMMIT_DATE "2021-Jul-9 12:34:15-0700"  
+#define SOURCE_COMMIT_HASH "b9ab7d3ca3f479c27ecceb6a6521a24a6dc6a5ad"  
