a678ff77f0426a20ad5f0b324b4b6c5889b4f502
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2021-07-11 12:20:43 -0700

    Fixed agent crash in WebRTC

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,5 +1,3 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2021-Jul-9 13:10:40-0700"  
-#define SOURCE_COMMIT_DATE "2021-Jul-9 13:10:40-0700"  
-#define SOURCE_COMMIT_HASH "51646002cd6d07c9d2ace22d3f526aeb33135734"  
-#define SOURCE_COMMIT_HASH "51646002cd6d07c9d2ace22d3f526aeb33135734"  
+#define SOURCE_COMMIT_DATE "2021-Jul-11 11:42:24-0700"  
+#define SOURCE_COMMIT_HASH "5618516cb96f22a24ae06fc910a61cfeb2d9eaa9"  
