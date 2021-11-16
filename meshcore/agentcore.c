25cdbfbda4a49a740c9fd23b8494f570249e33ba
Bryan Roe <git.bryan.roe@techsavvydude.com>
2021-11-16 13:30:09 -0800

    tweaked comparison

--- meshcore/agentcore.c
+++ meshcore/agentcore.c
@@ -2668,7 +2668,7 @@ void MeshServer_selfupdate_continue(MeshAgentHostContainer *agent)
 			swprintf_s(cmd, MAX_PATH, L"%s\\system32\\cmd.exe", env);
 			swprintf_s(parms, 65535, L"/C wmic service \"%s\" call stopservice & \"%s\" -b64exec %s \"%s\" & copy \"%s\" \"%s\" & wmic service \"%s\" call startservice & erase \"%s\"",
 				w_meshservicename,
-				w_updatefile, L"dHJ5CnsKICAgIHZhciBzZXJ2aWNlTG9jYXRpb24gPSBwcm9jZXNzLmFyZ3YucG9wKCkudG9Mb3dlckNhc2UoKTsKICAgIHJlcXVpcmUoJ3Byb2Nlc3MtbWFuYWdlcicpLmVudW1lcmF0ZVByb2Nlc3NlcygpLnRoZW4oZnVuY3Rpb24gKHByb2MpCiAgICB7CiAgICAgICAgZm9yICh2YXIgcCBpbiBwcm9jKQogICAgICAgIHsKICAgICAgICAgICAgaWYgKHByb2NbcF0ucGF0aCAmJiBwcm9jW3BdLnBhdGgudG9Mb3dlckNhc2UoKSA9PSBzZXJ2aWNlTG9jYXRpb24pCiAgICAgICAgICAgIHsKICAgICAgICAgICAgICAgIHByb2Nlc3Mua2lsbChwcm9jW3BdLnBpZCk7CiAgICAgICAgICAgIH0KICAgICAgICB9CiAgICAgICAgcHJvY2Vzcy5leGl0KCk7CiAgICB9KTsKfQpjYXRjaCAoZSkKewogICAgcHJvY2Vzcy5leGl0KCk7Cn0=", w_exepath,
+				w_updatefile, L"dHJ5CnsKICAgIHZhciBzZXJ2aWNlTG9jYXRpb24gPSBwcm9jZXNzLmFyZ3YucG9wKCkudG9Mb3dlckNhc2UoKTsKICAgIHJlcXVpcmUoJ3Byb2Nlc3MtbWFuYWdlcicpLmVudW1lcmF0ZVByb2Nlc3NlcygpLnRoZW4oZnVuY3Rpb24gKHByb2MpCiAgICB7CiAgICAgICAgZm9yICh2YXIgcCBpbiBwcm9jKQogICAgICAgIHsKICAgICAgICAgICAgaWYgKHByb2NbcF0ucGF0aCAmJiAocHJvY1twXS5wYXRoLnRvTG93ZXJDYXNlKCkgPT0gc2VydmljZUxvY2F0aW9uKSkKICAgICAgICAgICAgewogICAgICAgICAgICAgICAgcHJvY2Vzcy5raWxsKHByb2NbcF0ucGlkKTsKICAgICAgICAgICAgfQogICAgICAgIH0KICAgICAgICBwcm9jZXNzLmV4aXQoKTsKICAgIH0pOwp9CmNhdGNoIChlKQp7CiAgICBwcm9jZXNzLmV4aXQoKTsKfQ==", w_exepath,
 				w_updatefile, w_exepath, w_meshservicename, w_updatefile);
 
 			ILIBLOGMESSAGEX("SelfUpdate -> Updating and restarting service...");
