e6c3b72b937b139c10b6438740155279f8775ba9
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2022-03-23 14:35:31 -0700

    Added parentheses.

--- meshcore/agentcore.c
+++ meshcore/agentcore.c
@@ -872,7 +872,7 @@ ILibTransport_DoneState ILibDuktape_MeshAgent_RemoteDesktop_KVM_WriteSink(char *
 	}
 #endif
 
-	if (bufferLen > 4 && ntohs(((unsigned short*)buffer)[0]) == MNG_DEBUG)
+	if ((bufferLen > 4) && (ntohs(((unsigned short*)buffer)[0]) == MNG_DEBUG))
 	{
 		Duktape_Console_LogEx(ptrs->ctx, ILibDuktape_LogType_Info1, "%s", buffer + 4);
 	}
