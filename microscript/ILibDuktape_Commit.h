0ded08cd98cd3aceee6254357e4ad53d99785c77
Kirill Gontar <Kirill Gontar>
2025-08-21 19:39:24 +0200

    Force quit by ctrl c

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2025-Aug-21 18:38:37+0200"
-#define SOURCE_COMMIT_HASH "586a63a8f08db2ff355d47dd9b23c5149ed80b56"
+#define SOURCE_COMMIT_DATE "2025-Aug-21 19:19:14+0200"
+#define SOURCE_COMMIT_HASH "484bdb769a6d928cd072e31b4a544f698df811e0"
