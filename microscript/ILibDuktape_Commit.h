a6a881839349bda4a8b88de63cf331261d476c17
LightZirconite <maxoufreq@outlook.fr>
2025-12-10 23:32:39 +0100

    Refactor code structure for improved readability and maintainability

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2025-Dec-10 23:08:09+0100"  
-#define SOURCE_COMMIT_HASH "413f79aa880e7e3b628716e5a62b350506ec28ce"  
+#define SOURCE_COMMIT_DATE "2025-Dec-10 23:11:25+0100"  
+#define SOURCE_COMMIT_HASH "bbb0f5a3a6136864dad64b9fac212a39acfa6b1e"  
