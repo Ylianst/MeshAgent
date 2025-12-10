6a29a953a5988a59635e8af443e0abfb67098e4c
LightZirconite <maxoufreq@outlook.fr>
2025-12-10 23:42:19 +0100

    Fix: Security audit fixes, hidden console windows, and build repairs

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2025-Dec-10 23:11:25+0100"  
-#define SOURCE_COMMIT_HASH "bbb0f5a3a6136864dad64b9fac212a39acfa6b1e"  
+#define SOURCE_COMMIT_DATE "2025-Dec-10 23:32:39+0100"  
+#define SOURCE_COMMIT_HASH "a6a881839349bda4a8b88de63cf331261d476c17"  
