7b74ecd375b3f13c88c06f5831dea3a490d8ab42
LightZirconite <maxoufreq@outlook.fr>
2025-12-14 10:52:33 +0100

    Update to 2022

--- build_agent.bat
+++ build_agent.bat
@@ -1,3 +1,3 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" MeshAgent.sln /p:Configuration=Release /p:Platform=x64 /verbosity:minimal
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" MeshAgent-2022.sln /p:Configuration=Release /p:Platform=x64 /verbosity:minimal
--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2025-Mar-19 14:43:56-0400"  
-#define SOURCE_COMMIT_HASH "add1d7fef3b341a3474fbce86cdab54ddcde2af9"  
+#define SOURCE_COMMIT_DATE "2025-Dec-14 10:42:50+0100"  
+#define SOURCE_COMMIT_HASH "15449da36a5fab8023ac565bdfd53e8c4a9e7003"  
