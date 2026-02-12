ac59c2286652793a9ff23338e51b7c48d7e26bdf
Simon Smith <simonsmith5521@gmail.com>
2026-02-12 17:57:23 +0000

    update duktape commit date/hash

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
-// This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2025-Mar-6 21:44:07+0000"
-#define SOURCE_COMMIT_HASH "28d67274264e140e0c7a4254ad0e32865d5c4a13"
+// This file is auto-generated, any edits may be overwritten 
+#define SOURCE_COMMIT_DATE "2026-Feb-12 18:32:43+0100"  
+#define SOURCE_COMMIT_HASH "609a2ce4c4dd237eeebb86df9fafef6f6bc73949"  
