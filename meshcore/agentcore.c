3e6203d96b4d6993db9a3470e0a0f04ab7d26b6f
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2026-02-15 16:27:07 -0800

    Code safely fix

--- meshcore/agentcore.c
+++ meshcore/agentcore.c
@@ -4131,11 +4131,9 @@ void MeshServer_ConnectEx(MeshAgentHostContainer *agent)
 	}
 
 	// Set User-Agent for proxies to identify agents and versions
-	const char* FieldData = "MeshAgent ";
-	char combined[40];
-	strcpy(combined, FieldData);
-	strcat(combined, SOURCE_COMMIT_DATE);
-	ILibAddHeaderLine(req, "User-Agent", 10, combined, (int)strnlen_s(combined, 50));
+	char combined[sizeof("MeshAgent ") + sizeof(SOURCE_COMMIT_DATE)];
+	int combinedLen = sprintf_s(combined, sizeof(combined), "MeshAgent %s", SOURCE_COMMIT_DATE);
+	ILibAddHeaderLine(req, "User-Agent", 10, combined, combinedLen);
 
 	free(path);
 
--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,5 @@
 // This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2026-Feb-12 18:32:43+0100"  
-#define SOURCE_COMMIT_HASH "609a2ce4c4dd237eeebb86df9fafef6f6bc73949"  
+#define SOURCE_COMMIT_DATE "2026-Feb-14 22:23:26+0100"  
+#define SOURCE_COMMIT_DATE "2026-Feb-14 22:23:26+0100"  
+#define SOURCE_COMMIT_HASH "4f9692cee465d489072c5be74f8e17a6c676ed76"  
+#define SOURCE_COMMIT_HASH "4f9692cee465d489072c5be74f8e17a6c676ed76"  
