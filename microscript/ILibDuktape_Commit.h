f2302c12a627a2c69c93a0756b6ff916190b066f
Simon Smith <simonsmith5521@gmail.com>
2026-06-02 20:04:48 +0100

    update modules #332
    Signed-off-by: Simon Smith <simonsmith5521@gmail.com>

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
-// This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Jun-2 08:28:25-0500"
-#define SOURCE_COMMIT_HASH "a20c31f9e8925b84206ca21de45d610f35d5468b"
+// This file is auto-generated, any edits may be overwritten 
+#define SOURCE_COMMIT_DATE "2026-Jun-2 18:46:11+0200"  
+#define SOURCE_COMMIT_HASH "f3e031a7877f0a86ecf581b4031e11c0ed03dd8a"  
