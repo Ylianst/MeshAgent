f58dea0e548c86fe191bb86806fa2c0c5e76eb86
PTR <47147771+PTR-inc@users.noreply.github.com>
2026-08-05 08:12:27 +0100

    Add build files to .gitignore (#374)
    * Add build files to .gitignore
    
    * Remove build file

--- microscript/ILibDuktape_Commit.h
+++
@@ -1,3 +0,0 @@
-// This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2026-Jul-5 16:13:12+0100"  
-#define SOURCE_COMMIT_HASH "331b708372a02428b395db71ce507905bdf4016e"  
